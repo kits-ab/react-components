d790a7fd61fa53b036265bedb243c28365a4418e
Joakim Kemeny <joakim@kemeny.se>
2018-09-29 14:55:54 +0200

    Change the way we handle icons and add some more

--- src/icons/LocationIcon.tsx
+++ src/icons/LocationIcon.tsx
@@ -3,13 +3,13 @@ import * as React from "react"
 
 import * as types from "../types"
 
-export class ProjectIcon extends React.PureComponent<types.IconProps> {
+export class LocationIcon extends React.PureComponent<types.IconProps> {
   render() {
     const props = this.props
     return (
       <svg viewBox="0 0 24 24" width="24" height="24" {...props}>
         <path
-          d="M21.6 5.8h-4.8V3.4c0-1.332-1.068-2.4-2.4-2.4H9.6a2.392 2.392 0 0 0-2.4 2.4v2.4H2.4A2.382 2.382 0 0 0 .012 8.2L0 21.4c0 1.332 1.068 2.4 2.4 2.4h19.2c1.332 0 2.4-1.068 2.4-2.4V8.2c0-1.332-1.068-2.4-2.4-2.4zm-7.2 0H9.6V3.4h4.8v2.4z"
+          d="M12 24s9-8.373 9-15c0-6.627-4.029-9-9-9S3 2.373 3 9s9 15 9 15zM7.5 7.5a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0z"
           fill="inherit"
           fillRule="nonzero"
         />
