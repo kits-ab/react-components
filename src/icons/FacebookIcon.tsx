e0ce38ce22f6a946bcfa5192f220a240609e2d9f
Pontus Doverstav <pontus.doverstav@kits.se>
2022-08-29 21:44:48 +0200

    Regenerate icon components with SVGR v6

--- src/icons/FacebookIcon.tsx
+++ src/icons/FacebookIcon.tsx
@@ -2,9 +2,9 @@ import React from "react"
 import * as types from "../types"
 export const FacebookIcon = (props: types.IconProps) => {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" {...props}>
+    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
-        d="M8.481 24h4.967V11.999h3.313l.439-4.136h-3.752l.005-2.07c0-1.078.103-1.657 1.653-1.657h2.07V0h-3.314c-3.98 0-5.38 2.007-5.38 5.38v2.483H6V12h2.481V24z"
+        d="M8.481 24h4.967V11.999h3.313l.439-4.136h-3.752l.005-2.07c0-1.078.103-1.657 1.653-1.657h2.07V0h-3.314c-3.98 0-5.38 2.007-5.38 5.38v2.483H6V12h2.481V24Z"
         fill="inherit"
         fillRule="nonzero"
       />
--- src/icons/NewWindowIcon.tsx
+++ src/icons/NewWindowIcon.tsx
@@ -2,10 +2,10 @@ import React from "react"
 import * as types from "../types"
 export const NewWindowIcon = (props: types.IconProps) => {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" {...props}>
+    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g fill="inherit" fillRule="nonzero">
-        <path d="M9 3v12h12V3H9zM8 0h14a2 2 0 012 2v14a2 2 0 01-2 2H8a2 2 0 01-2-2V2a2 2 0 012-2z" />
-        <path d="M13.5 21a1.5 1.5 0 010 3h-12A1.5 1.5 0 010 22.5v-12a1.5 1.5 0 013 0V21h10.5z" />
+        <path d="M9 3v12h12V3H9ZM8 0h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2Z" />
+        <path d="M13.5 21a1.5 1.5 0 0 1 0 3h-12A1.5 1.5 0 0 1 0 22.5v-12a1.5 1.5 0 0 1 3 0V21h10.5Z" />
       </g>
     </svg>
   )
